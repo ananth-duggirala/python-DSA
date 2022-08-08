4cdc94db1eafae9bdb97397f05916a5f6ec5f77d
Ananth Duggirala <ananth.duggirala@gmail.com>
2022-08-07 22:00:36 -0700

    dynamic programming

--- everything.py
+++ everything.py
@@ -67,5 +67,81 @@ class Deque:
 # List(), add(item), remove(item), search(item), isEmpty(), size(), append(item), index(item), insert(pos, item), pop(), pop(pos)
 
 # Linked list implementation
-# Create a Node class: getData, getNext, setData, setNext
-# Create an UnorderedList class: 
+# Create a Node class: Node(), getData, getNext, setData, setNext
+# Create an UnorderedList class: UnorderedList(), isEmpty(), add(item), size(), search(item), remove(item) 
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+	
+	def getData(self):
+		return self.data
+
+	def setData(self, data):
+		self.data = data
+
+	def getNext(self):
+		return self.next
+
+	def setNext(self, nxt):
+		self.next = nxt
+
+class UnorderedList:
+	def __init__(self):
+		self.head = None
+
+	def isEmpty(self):
+		return self.head == None
+
+	def add(self, item):
+		temp = Node(item)
+		temp.setNext(self.head)
+		self.head = temp
+
+	def size(self):
+		current = self.head
+		size = 0
+		while current != None:
+			size += 1
+			current = current.next
+		return size
+
+	def search(self, item):
+		current = self.head
+		found = False
+		while current != None and not found:
+			if current.getData() == item:
+				found = True
+			else:
+				current = current.getNext()
+
+		return found
+
+	def remove(self, item):
+		current = self.head
+		previous = None
+		found = False
+		while not found:
+			if current.getData() == item:
+				found = True
+			else:
+				previous = current
+				current = current.getNext()
+
+		if previous == None:
+			self.head = current.getNext()
+		else:
+			previous.setNext(current.getNext())
+
+# TODO: MazeRunner
+
+# Dynamic programming: making change
+
+def dpMakeChange(coinValueList, change, minCoins):
+	for cents in range(change+1):
+		coinCount = cents
+		for j in [c for c in coinValueList if c <= cents]:
+			if minCoins[cents-j] + 1 < coinCount:
+				coinCount = minCoins[cents-j] + 1
+		minCoins[cents] = coinCount
+	return minCoins[change]
